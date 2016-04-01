ff56c2bae9f224178c9013dd7082038fda7d6c01
Marine Commerçon <marine.commercon@qualia.fr>
2016-04-01 14:15:10 +0200

    MapWize in ViewController

--- WorkspaceManagement/ViewController.h
+++ WorkspaceManagement/ViewController.h
@@ -6,11 +6,15 @@
 //  Copyright © 2016 docapost. All rights reserved.
 //
 
+@import Mapwize;
 #import <UIKit/UIKit.h>
+#import "CoreLocation/CoreLocation.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MWZMapDelegate>
 
+@property (strong, nonatomic)CLLocationManager* locationManager;
 @property (strong, nonatomic) IBOutlet UIView *container;
+@property (weak, nonatomic) IBOutlet MWZMapView *myMapView;
+
 
 @end
-
--- WorkspaceManagement/ViewController.m
+++ WorkspaceManagement/ViewController.m
@@ -18,6 +18,8 @@
 }
 
 @synthesize container;
+@synthesize myMapView;
+@synthesize locationManager;
 
 - (void)viewDidLoad
 {
@@ -26,6 +28,32 @@
     
     floor = 1;
     
+    [self initMap];
+
+
+    
+}
+
+- (void)initMap
+{
+    
+    locationManager = [[CLLocationManager alloc] init];
+    CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+    
+    if (status == kCLAuthorizationStatusAuthorizedAlways || status ==kCLAuthorizationStatusAuthorizedWhenInUse) {
+        NSLog(@"Location Authorization granted");
+    } else {
+        NSLog(@"Requesting Location Authorization");
+        [locationManager requestWhenInUseAuthorization];
+    }
+    
+    myMapView = (MWZMapView*)[self myMapView];
+    MWZMapOptions* options = [[MWZMapOptions alloc] init];
+    options.apiKey = @"f1a08de88e9353ed5a9e97a0f89bdxdf";
+    myMapView.delegate = self;
+    [myMapView loadMapWithOptions: options];
+    
+    [myMapView fitBounds:[[MWZLatLonBounds alloc] initWithNorthEast:[[MWZLatLon alloc] initWithLatitude:@48.82633358744936 longitude:@2.259978353977203] southWest:[[MWZLatLon alloc] initWithLatitude:@48.8254489062897 longitude:@2.258988618850708]]];
 }
 
 - (void)initContainerSwipeGesture
@@ -59,16 +87,19 @@
             frame.origin.y = CGRectGetHeight([UIScreen mainScreen].bounds) / 1.13;
             container.frame = frame;
             floor = 1;
+            self.myMapView.userInteractionEnabled = true;
             break;
         case (1):
             frame.origin.y = CGRectGetHeight([UIScreen mainScreen].bounds) / 1.3;
             container.frame = frame;
             floor = 2;
+            self.myMapView.userInteractionEnabled = false;
             break;
         case (2):
             frame.origin.y = CGRectGetHeight([UIScreen mainScreen].bounds) / 3;
             container.frame = frame;
             floor = 3;
+            self.myMapView.userInteractionEnabled = false;
             break;
         default:
             NSLog(@"Error");
@@ -86,21 +117,76 @@
             frame.origin.y = CGRectGetHeight([UIScreen mainScreen].bounds) / 1.13;
             container.frame = frame;
             floor = 0;
+            self.myMapView.userInteractionEnabled = true;
             break;
         case (2):
             frame.origin.y = CGRectGetHeight([UIScreen mainScreen].bounds) / 1.13;
             container.frame = frame;
             floor = 1;
+            self.myMapView.userInteractionEnabled = false;
             break;
         case (3):
             frame.origin.y = CGRectGetHeight([UIScreen mainScreen].bounds) / 1.3;
             container.frame = frame;
             floor = 1;
+            self.myMapView.userInteractionEnabled = false;
             break;
         default:
             NSLog(@"Error");
             break;
     }
+}
+
+//Listering to delegate events
+- (void) map:(MWZMapView*) map didClick:(MWZLatLon*) latlon {
+    
+}
+
+- (void) map:(MWZMapView*) map didClickOnPlace:(MWZPlace*) place {
+    [myMapView access: @"h14aEHT8O6OvxoNo"];
+    [myMapView centerOnCoordinates:@48.82594334079911 longitude:@2.259484827518463 floor:@5 zoom:@19];
+}
+
+- (void) map:(MWZMapView*) map didClickOnVenue:(MWZVenue*) venue {
+   
+}
+
+- (void) map:(MWZMapView*) map didClickOnMarker:(MWZPosition *)marker {
+  
+}
+
+- (void) map:(MWZMapView*) map didChangeFloor:(NSNumber*) floor {
+
+}
+
+- (void) map:(MWZMapView*) map didChangeFloors:(NSArray*) floors {
+
+}
+
+- (void) map:(MWZMapView *)map didMove:(MWZLatLon *)center {
+
+}
+
+- (void) map:(MWZMapView*) map didFailWithError: (NSError *)error {
+   
+}
+
+- (void) map:(MWZMapView*) map didChangeFollowUserMode:(BOOL)followUserMode {
+}
+
+- (void) map:(MWZMapView*) map didChangeUserPosition:(MWZMeasurement *)userPosition {
+}
+
+- (void )map:(MWZMapView*) map didChangeZoom: (NSNumber*) zoom {
+}
+
+- (void )map:(MWZMapView*) map didClickLong: (MWZLatLon*) latlon {
+}
+
+- (void) map:(MWZMapView*) map didStartDirections: (NSString*) infos {
+}
+
+- (void )map:(MWZMapView*) map didStopDirections: (NSString*) infos {
 }
 
 - (void)didReceiveMemoryWarning
