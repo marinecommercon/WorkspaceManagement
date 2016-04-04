f6dcb7e4d5395288de5e9326adf5c4d6df52fa68
Daoud Lyess <daoud_l@etna-alternance.net>
2016-04-04 12:05:50 +0200

    Add POD "CCMPopup" + PopupDetailController

---
+++ WorkspaceManagement/PopupDetailController.h
@@ -0,0 +1,13 @@
+//
+//  PopupViewController.h
+//  WorkspaceManagement
+//
+//  Created by Lyess on 04/04/2016.
+//  Copyright © 2016 docapost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopupDetailController : UIViewController
+
+@end
---
+++ WorkspaceManagement/PopupDetailController.m
@@ -0,0 +1,37 @@
+//
+//  PopupViewController.m
+//  WorkspaceManagement
+//
+//  Created by Lyess on 04/04/2016.
+//  Copyright © 2016 docapost. All rights reserved.
+//
+
+#import "PopupDetailController.h"
+
+@interface PopupDetailController ()
+
+@end
+
+@implementation PopupDetailController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
